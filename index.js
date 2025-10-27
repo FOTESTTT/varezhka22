1be2bfba7abbb5e97b478af01bb63092306a296c
FOTESTTT <133633710+FOTESTTT@users.noreply.github.com>
2025-10-27 12:19:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ index.js
@@ -0,0 +1,12 @@
+// Плавное появление при прокрутке
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, { threshold: 0.2 });
+
+sections.forEach(sec => observer.observe(sec));
